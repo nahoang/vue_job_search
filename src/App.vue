67974e1a6447280dd7eaddacc4e20e9c3cf23470
nahoang <nahoang9698@gmail.com>
2023-06-11 14:59:45 +0700

    feat: Implement MainNav component

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,16 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Welcome to Your Vue.js App" />
+  <MainNav></MainNav>
 </template>
 
 <script>
 import HelloWorld from "./components/HelloWorld.vue";
+import MainNav from "./components/MainNav.vue";
 
 export default {
   name: "App",
   components: {
     HelloWorld,
+    MainNav,
   },
 };
 </script>
